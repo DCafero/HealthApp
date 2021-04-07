0c159666fa0d1cfc377451d5170511651b5e744f
DCafero <cafero56@students.rowan.edu>
2021-04-07 14:31:11 -0400

    added login

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import MainBody from "./Components/MainBody";
+import SignIn from "./Components/Sign-in/Sign-in";
+
+let signedIn = true;
 
 function App() {
-  return <MainBody></MainBody>;
+  return signedIn ? <MainBody></MainBody> : <SignIn></SignIn>;
 }
 
 export default App;
