fa0064bbe83f8b0bf61688e3559a0062f83bf042
LuLaValva <lulavalva@gmail.com>
2021-04-20 15:42:47 -0400

    Added basic communication with server

--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/hello_world", (req, res) => {
-  res.send("First Fetch");
+  res.send("Information from Server");
 });
 
 app.listen(5000, () => console.log("App Listening on port 5000..."));
